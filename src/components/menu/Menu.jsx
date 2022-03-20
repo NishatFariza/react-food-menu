e3835773b9fe513abb3554ff758cbc6fe57aed83
NishatFariza <nishatfarzia78@gmail.com>
2022-03-21 03:31:32 +0600

    display food

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,10 +1,10 @@
 
 import React from 'react';
-import { Button, Card } from 'react-bootstrap';
+import {Button, Card } from 'react-bootstrap';
 import './Menu.css'
 
 const Menu = (props) => {
-    const {strMealThumb} =props.foodItem;
+    const {strMealThumb, strMeal, strInstructions} =props.foodItem;
     return (
         <div>
            {/* card item */}
@@ -16,10 +16,10 @@ const Menu = (props) => {
                   <div className='card-body'>
                       <div>
                           <div className='card-title-wrap'>
-                              <h4 className='card-title'>Buttermilk Pancakes</h4>
+                              <h4 className='card-title'>{strMeal}</h4>
                               <span className='food-price'>$15</span>
                           </div>
-                          <p className='card-description'>I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed</p>
+                          <p className='card-description'>{strInstructions.slice(0, 200) +"....See More"}</p>
                       </div>
                   </div>
 
